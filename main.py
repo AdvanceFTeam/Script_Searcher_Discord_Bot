0e2739a38d2f90ec67647c526015a1820aa6954f
Steven <116656099+StevenK-293@users.noreply.github.com>
2024-05-27 00:23:47 -0400

    Lots of feature added
    - New Design
    - More error handling
    - Fix error when you can find script
    - Added Universal Script or Status
    - Etc

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ intents.typing = False
 intents.presences = False
 intents.message_content = True
 
-
 class MyBot(commands.Bot):
 
     def __init__(self, *args, **kwargs):
@@ -26,7 +25,6 @@ class MyBot(commands.Bot):
 
     async def setup_hook(self):
         await self.tree.sync()
-
 
 bot = MyBot(command_prefix='!', intents=intents)
 
@@ -38,14 +36,10 @@ async def on_ready():
     await bot.change_presence(activity=activity)
     print("Bot is ready 🤖")
 
-
-# Prefix command
 @bot.command()
 async def search(ctx, query=None, mode='free'):
     await execute_search(ctx, query, mode, prefix=True)
 
-
-# Slash command
 @bot.tree.command(name="search", description="Search for scripts")
 @app_commands.describe(query="The search query",
                        mode="Search mode ('free' or 'paid')")
@@ -55,38 +49,38 @@ async def slash_search(interaction: discord.Interaction,
     ctx = await bot.get_context(interaction)
     await execute_search(ctx, query, mode, prefix=False)
 
-
 async def execute_search(ctx, query, mode, prefix):
     user_id = ctx.author.id
-    if user_id in bot.active_searches:
-        message = await ctx.send("You already have an active search running. Please wait for the 1st command to stop running.")
-        await asyncio.sleep(random.randint(5, 10))
-        await message.delete()
-        return
-
-    bot.active_searches[user_id] = True
-
-    if query is None:
-        help_message = (
-            "Search for scripts using the `!search` command followed by your query.\n\n"
-            "You can also specify the search mode (default is `'free'`).\n"
-            "Modes: `'free'`, `'paid'`\n"
-            "Example: `!search arsenal paid`\n\n"
-            "Please provide a query to get started.")
-        initial_embed = discord.Embed(title="🔍 Script Search Help",
-                                      description=help_message,
-                                      color=0x3498db)
-        initial_embed.set_thumbnail(
-            url="https://media1.tenor.com/m/j9Jhn5M1Xw0AAAAd/neuro-sama-ai.gif"
-        )
-        await ctx.send(embed=initial_embed)
-        del bot.active_searches[user_id]
-        return
-
-    page = 1
-    api_url = f"https://scriptblox.com/api/script/search?q={query}&mode={mode}&page={page}"
-
     try:
+        if user_id in bot.active_searches:
+            message = await ctx.send("You already have an active search running. Please wait for the first command to complete.")
+            await asyncio.sleep(random.randint(5, 10))
+            await message.delete()
+            return
+
+        bot.active_searches[user_id] = True
+
+        if query is None:
+            help_message = (
+                "Use `!search <query>` to find scripts.\n\n"
+                "You can specify the search mode (default is `free`).\n"
+                "**Modes**: `free`, `paid`\n"
+                "**Example**: `!search arsenal paid`\n\n"
+                "Please provide a query to get started."
+            )
+            initial_embed = discord.Embed(
+                title="🔍 Script Search Help",
+                description=help_message,
+                color=0x3498db
+            )
+            initial_embed.set_thumbnail(url="https://media1.tenor.com/m/j9Jhn5M1Xw0AAAAd/neuro-sama-ai.gif")
+            await ctx.send(embed=initial_embed)
+            del bot.active_searches[user_id]
+            return
+
+        page = 1
+        api_url = f"https://scriptblox.com/api/script/search?q={query}&mode={mode}&page={page}"
+
         response = requests.get(api_url)
         response.raise_for_status()
         data = response.json()
@@ -101,14 +95,16 @@ async def execute_search(ctx, query, mode, prefix):
 
             message = await ctx.send("Fetching data...")
 
-            await display_scripts(ctx, message, scripts, page,
-                                  data["result"]["totalPages"], prefix)
+            await display_scripts(ctx, message, scripts, page, data["result"]["totalPages"], prefix)
         else:
             await ctx.send("No scripts found!")
     except requests.RequestException as e:
         await ctx.send(f"An error occurred: {e}")
+    except KeyError as ke:
+        await ctx.send(f"An error occurred while processing your request. Please try again later. Error: {ke}")
     finally:
-        del bot.active_searches[user_id]
+        if user_id in bot.active_searches:
+            del bot.active_searches[user_id]
 
 
 async def display_scripts(ctx, message, scripts, page, total_pages, prefix):
@@ -128,9 +124,7 @@ async def display_scripts(ctx, message, scripts, page, total_pages, prefix):
             return user == ctx.author and str(reaction.emoji) in ["⬅️", "➡️"]
 
         try:
-            reaction, _ = await bot.wait_for("reaction_add",
-                                             check=check,
-                                             timeout=30.0)
+            reaction, _ = await bot.wait_for("reaction_add", check=check, timeout=30.0)
 
             if str(reaction.emoji) == "⬅️" and page > 1:
                 page -= 1
@@ -146,7 +140,6 @@ async def display_scripts(ctx, message, scripts, page, total_pages, prefix):
                 await ctx.send("You took too long to interact.", ephemeral=True)
             break
 
-
 def create_embed(script, page, total_pages):
     game_name = script["game"]["name"]
     game_id = script["game"]["gameId"]
@@ -158,62 +151,57 @@ def create_embed(script, page, total_pages):
     has_key = script.get("key", False)
     key_link = script.get("keyLink", "")
     is_patched = script.get("isPatched", False)
+    is_universal = script.get("isUniversal", False)
     created_at = script["createdAt"]
     updated_at = script["updatedAt"]
-    game_image_url = "https://scriptblox.com" + script["game"].get(
-        "imageUrl", "")
+    game_image_url = "https://scriptblox.com" + script["game"].get("imageUrl", "")
 
-    paid_or_free = "**Free** 💰" if script_type == "free" else "**Paid** 💲"
-    views_emoji = "**👀**"
-    verified_emoji = "**Verified** ✅" if verified else "**Not Verified** ❌"
-    key_emoji = "**Key** 🔑" if has_key else "**No Key** ❌"
+    paid_or_free = "Free" if script_type == "free" else "💲 Paid"
+    verified_status = "✅ Verified" if verified else "❌ Not Verified"
+    key_status = f"[Key Link]({key_link})" if has_key and key_link else ("🔑 Has Key" if has_key else "✅ No Key")
+    patched_status = "❌ Patched" if is_patched else "✅ Not Patched"
+    universal_status = "🌐 Universal" if is_universal else "Not Universal"
+    truncated_script_content = (script_content[:max_content_length - 3] + "..." if len(script_content) > max_content_length else script_content)
 
-    if key_link:
-        key_text = f"[**Key Link**]({key_link})"
-    else:
-        key_text = key_emoji
+    embed = discord.Embed(title=title, color=0x206694)
 
-    is_patched_emoji = "**Patched** ✅" if is_patched else "**Not Patched** ❌"
+    embed.add_field(name="Game", value=f"[{game_name}](https://www.roblox.com/games/{game_id})", inline=True)
+    embed.add_field(name="Verified", value=verified_status, inline=True)
+    embed.add_field(name="Script Type", value=paid_or_free, inline=True)
+    embed.add_field(name="Universal", value=universal_status, inline=True)
+    embed.add_field(name="Views", value=f"👁️ {views}", inline=True)
+    embed.add_field(name="Key", value=key_status, inline=True)
+    embed.add_field(name="Patched", value=patched_status, inline=True)
+    embed.add_field(name="Script Content", value=f"```lua\n{truncated_script_content}\n```", inline=False)
+    embed.add_field(name="", value=f"**Created At:** {created_at}\n**Updated At:** {updated_at}", inline=False)
 
-    truncated_script_content = (script_content[:max_content_length - 3] +
-                                "..." if len(script_content)
-                                > max_content_length else script_content)
+    set_image_or_thumbnail(embed, game_image_url)
 
-    field_value = (
-        f"**Game**: [{game_name}](https://www.roblox.com/games/{game_id})\n"
-        f"**Verified**: {verified_emoji}\n"
-        f"**Script Type**: {paid_or_free}\n"
-        f"**Views**: {views} {views_emoji}\n\n"
-        f"**Key**: {key_text}\n"
-        f"**Patched**: {is_patched_emoji}\n\n"
-        f"**Created At**: {created_at}\n"
-        f"**Updated At**: {updated_at}\n"
-        f"```lua\n{truncated_script_content}\n```")
+    embed.set_footer(text=f"Made by AdvanceFalling Team | Page {page}/{total_pages}", 
+                     icon_url="https://img.getimg.ai/generated/img-u1vYyfAtK7GTe9OK1BzeH.jpeg")
+    
+    return embed
 
-    embed = discord.Embed(title=title, description=field_value, color=0x27ae60)
-
+def set_image_or_thumbnail(embed, url):
+    try:
+        if url and validators.url(url):
+            embed.set_image(url=url)
+        else:
+            embed.set_image(url="https://c.tenor.com/jnINmQlMNbsAAAAC/tenor.gif")
+    except Exception as e:
+        print(f"Error setting image URL: {e}")
+        embed.set_image(url="https://c.tenor.com/jnINmQlMNbsAAAAC/tenor.gif")
+        
+        
+    ''' Old Code you can use: if you want the image or thumbnail to be smaller
     try:
         if game_image_url and validators.url(game_image_url):
             embed.set_thumbnail(url=game_image_url)
         else:
-            embed.set_thumbnail(
-                url=
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8U6yuDVz_6IYqS9cM2oJpGzrM9o-hZT_k21aqQclWBA&s"
-            )
+            embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8U6yuDVz_6IYqS9cM2oJpGzrM9o-hZT_k21aqQclWBA&s")
     except Exception as e:
         print(f"Error setting thumbnail URL: {e}")
-        embed.set_thumbnail(
-            url=
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8U6yuDVz_6IYqS9cM2oJpGzrM9o-hZT_k21aqQclWBA&s"
-        )
-
-    embed.set_footer(
-        text=f" Made by AdvanceFalling Team | Page {page}/{total_pages}",
-        icon_url=
-        "https://i.pinimg.com/564x/7e/ed/10/7eed10f9bef56d535f4e610d48d1a06b.jpg"
-    )
-
-    return embed
-
+        embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8U6yuDVz_6IYqS9cM2oJpGzrM9o-hZT_k21aqQclWBA&s")
+    '''
 
 bot.run(TOKEN)
