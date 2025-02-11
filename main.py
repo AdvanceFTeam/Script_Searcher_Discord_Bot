d00a6803fb72e220a3b101a736417c0b2679deb8
YellowGreg <sksor6@gmail.com>
2025-02-11 17:04:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def fetch_scripts(api, query, mode, page):
             params = {"q": query, "script name": query, "mode": mode, "page": page}
             query_string = urllib.parse.urlencode(params, safe=' ')
             url = f"https://scriptblox.com/api/script/search?{query_string}"
+            # just use my proxy api if the scriptblox api doesnt work
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
             }
